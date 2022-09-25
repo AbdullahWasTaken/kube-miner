8afebf39ab5367d9802ae09e11068a877a943992
Abdullah Khan <abdullah.khan4620@gmail.com>
2022-09-25 02:36:21 -0400

    Using dynamic client

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/AbdullahWasTaken/kube-miner/collector"
+import (
+	"flag"
+	"path/filepath"
+
+	"github.com/AbdullahWasTaken/kube-miner/collector"
+	"github.com/AbdullahWasTaken/kube-miner/config"
+	"k8s.io/client-go/util/homedir"
+)
 
 func main() {
-	collector.Workflow()
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	outputPath := flag.String("out", "State", "relative path to where the collected data will be stored")
+	flag.Parse()
+	var c *config.Config = config.BuildConfig(kubeconfig, *outputPath)
+	collector.GetClusterState(c)
 }
