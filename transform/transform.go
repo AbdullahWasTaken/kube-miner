8fe1c3d6f6d06ab3fbe4f992692484a35d5be633
revelo <abdullah.khan4620@gmail.com>
2022-10-23 08:45:41 -0400

    refactor

--- transform/transform.go
+++ transform/transform.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/itchyny/gojq"
-	log "github.com/sirupsen/logrus"
 )
 
 type triple struct {
@@ -31,61 +30,6 @@ func (tpl *triples) String() string {
 		sb.WriteString(v.String())
 	}
 	return sb.String()
-}
-
-func TargetRef(jsonStr []byte, rdfFilePath string) {
-	jqQuery := `select((.Object.kind | test("EndpointsList")) and (.items != null) and (.items[0] != null))
-				| [.items[] 
-					| .kind as $p1 
-					| (["_:", $p1,"-", .metadata.name]|add) as $s 
-					| .subsets[]? 
-					| .addresses[]? 
-					| select((.targetRef != null) and (.targetRef.kind != null) and (.targetRef.name != null)) 
-					| {"Subject": $s, "Predicate": ([$p1, "_", .targetRef.kind]|add) , "Object": (["_:", .targetRef.kind, "-", .targetRef.name]|add ) } ]`
-	if err := saveRdf(jqQuery, jsonStr, rdfFilePath); err != nil {
-		log.Error(err)
-	}
-	jqQuery = `select((.Object.kind | test("EndpointSliceList")) and (.items != null) and (.items[0] != null))
-				| [.items[] 
-					| .kind as $p1 
-					| (["_:", $p1, "-", .metadata.name]|add) as $s 
-					| .endpoints[]?
-					| select((.targetRef != null) and (.targetRef.kind != null) and (.targetRef.name != null)) 
-					| {"Subject": $s, "Predicate": ([$p1, "_", .targetRef.kind]|add) , "Object": (["_:", .targetRef.kind, "-", .targetRef.name]|add ) } ]`
-	if err := saveRdf(jqQuery, jsonStr, rdfFilePath); err != nil {
-		log.Error(err)
-	}
-}
-
-func NodeProp(jsonStr []byte, rdfFilePath string) {
-	jqQuery := `[.items[] 
-					| [paths(scalars) as $path 
-					| {"key": $path 
-					| join("_"), "value": getpath($path)}] 
-					| from_entries] 
-					| [.[] 
-					| (["_:", .kind, "-", .metadata_name]|add) as $s 
-					| to_entries[] 
-					| .key as $p 
-					| (.value|tostring) as $o 
-					| {"Subject":$s, "Predicate":$p, "Object":$o} ]`
-	if err := saveRdf(jqQuery, jsonStr, rdfFilePath); err != nil {
-		log.Error(err)
-	}
-}
-
-func OwnRef(jsonStr []byte, rdfFilePath string) {
-	jqQuery := `[.items[]? 
-					| .kind as $p1 
-					| (["_:", $p1, "-", .metadata.name]|add) as $src 
-					| [.metadata.ownerReferences[]? 
-					| .kind as $p2 
-					| (["_:", $p2, "-" ,.name]|add) as $dst 
-					| {"Subject":$src, "Predicate":([$p1,"_", $p2]|add), "Object":$dst}]] 
-					| flatten`
-	if err := saveRdf(jqQuery, jsonStr, rdfFilePath); err != nil {
-		log.Error(err)
-	}
 }
 
 func saveRdf(jqQuery string, jsonStr []byte, rdfFilepath string) error {
