eb8d23a4fd90dfe42b9fbcdeab1a861652c3c5ce
revelo <abdullah.khan4620@gmail.com>
2022-10-22 20:44:30 -0400

    state loader

---
+++ utils/utils.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Load all the resource states store in a json directory `jsonDir` and
+// returns a map containing json strings of loaded resource states.
+func LoadState(jsonDir string) map[string]interface{} {
+	cs := map[string]interface{}{}
+	files, err := os.ReadDir(jsonDir)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, f := range files {
+		jsonStr, err := os.ReadFile(filepath.Join(jsonDir, f.Name()))
+		if err != nil {
+			log.Error(err)
+		} else {
+			// add loaded object to cluster state `cs`
+			cs[strings.Split(f.Name(), ".")[0]] = jsonStr
+		}
+	}
+	return cs
+}
